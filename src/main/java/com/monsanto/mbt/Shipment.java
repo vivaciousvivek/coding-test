42f3a649c5855ee669130857022252a6ec89560c
vivaciousvivek <vvkvivek.singh@gmail.com>
2018-08-21 15:48:22 +0530

    Implementation of Shipment and its Junit test class of this given Problem.

--- src/main/java/com/monsanto/mbt/Shipment.java
+++ src/main/java/com/monsanto/mbt/Shipment.java
@@ -1,9 +1,70 @@
 package com.monsanto.mbt;
 
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * This program is for the shipping specialist that allows the specialist two options for creating
+ * shipments.
+ *
+ * <p>One of the features the program needs to have is to allow the specialist to sort the Widgets
+ * by date or by color to assist the specialist with shipment creation.
+ *
+ * <p>One of the constraints on shipment creation is that a shipment can hold a maximum of 10
+ * Widgets.
+ *
+ * @author VIVEK KUMAR SINGH
+ * @since (2018-08-21 13:18:13)
+ */
 public class Shipment {
+  /**
+   * This method will help the specialist to sort the widgets by date.
+   *
+   * @param widgets Takes the raw list of widgets as input.
+   * @param dateComparator Date comparator object for sorting according to passed comparator. This
+   *     parameter is optional, if user will not pass the comparator object them we will sort the
+   *     list in assenting order.
+   * @return According to constraint we can only hold 10 shipment. hence result will contains only
+   *     10 elements.
+   */
+  public List<Widget> sortWidgetsByDate(List<Widget> widgets, Comparator<Widget> dateComparator) {
+    if (widgets == null) throw new IllegalStateException("Input widgets can't be null");
 
+    Comparator<Widget> comparator = dateComparator;
+    if (dateComparator == null) {
+      //        This Comparator can also be written using lambda expression like so =>
+      //        comparator = (w1, w2) -> w1.getProductionDate().compareTo(w2.getProductionDate());
 
+      comparator = Comparator.comparing(Widget::getProductionDate);
+    }
 
+    widgets.sort(comparator);
 
+    return widgets.subList(0, 10);
+  }
 
+  /**
+   * This method will help the specialist to sort the widgets by color.
+   *
+   * @param widgets Takes the raw list of widgets as input.
+   * @param colorComparator Color comparator object for sorting according to passed comparator. This
+   *     parameter is optional, if user will not pass the comparator object them we will sort the
+   *     list in assenting order.
+   * @return According to constraint we can only hold 10 shipment. hence result will contains only
+   *     10 elements.
+   */
+  public List<Widget> sortWidgetsByColor(List<Widget> widgets, Comparator<Widget> colorComparator) {
+    if (widgets == null) throw new IllegalStateException("Input widgets can't be null");
+
+    Comparator<Widget> comparator = colorComparator;
+    if (colorComparator == null) {
+      //        This Comparator can also be written using lambda expression like so =>
+      //        comparator = (w1, w2) -> w1.getColor().compareTo(w2.getColor());
+      comparator = Comparator.comparing(Widget::getColor);
+    }
+
+    widgets.sort(comparator);
+
+    return widgets.subList(0, 10);
+  }
 }
--- src/test/java/com/monsanto/mbt/ShipmentTest.java
+++ src/test/java/com/monsanto/mbt/ShipmentTest.java
@@ -1,24 +1,137 @@
 package com.monsanto.mbt;
 
-
+import org.junit.Before;
 import org.junit.Test;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+/**
+ * @author VIVEK KUMAR SINGH
+ * @since (2018-08-21 13:56:38)
+ */
 public class ShipmentTest {
 
-    @Test
-    public void testShipment_Sorted_By_Color(){
-        //Implement your solution to get list of shipments
+  private Shipment shipment;
+  private List<Widget> widgets;
+  private List<String> sortedAscColors;
+  private List<String> sortedDescColors;
+  private List<Date> sortedAscDates;
+  private List<Date> sortedDescDates;
 
+  @Before
+  public void init() {
+    shipment = new Shipment();
+    widgets = WidgetUtils.getSampleWidgets();
+    sortedAscColors = widgets.stream().map(Widget::getColor).collect(Collectors.toList());
+
+    sortedDescColors = new ArrayList<>(sortedAscColors);
+
+    Collections.sort(sortedAscColors);
+    sortedDescColors.sort(Comparator.reverseOrder());
+
+    sortedAscDates = widgets.stream().map(Widget::getProductionDate).collect(Collectors.toList());
+
+    sortedDescDates = new ArrayList<>(sortedAscDates);
+
+    Collections.sort(sortedAscDates);
+    sortedDescDates.sort(Comparator.reverseOrder());
+  }
+
+  /** Sorted shipment by its color in ascending order */
+  @Test
+  public void testShipment_Sorted_By_Color() {
+    // Ascending order sorting test
+    List<Widget> sortedWidgetsInAscending =
+        shipment.sortWidgetsByColor(new ArrayList<>(widgets), null);
+
+    // test whether the sorted list not holding more than 10 elements.
+    assertTrue(sortedWidgetsInAscending.size() > 0 && sortedWidgetsInAscending.size() <= 10);
+
+    // test the list in ascending order by color
+    for (int i = 0; i < sortedWidgetsInAscending.size() - 1; i++) {
+      assertEquals(sortedAscColors.get(i), sortedWidgetsInAscending.get(i).getColor());
+
+      assertTrue(
+          sortedWidgetsInAscending
+                  .get(i)
+                  .getColor()
+                  .compareTo(sortedWidgetsInAscending.get(i + 1).getColor())
+              <= 0);
     }
+  }
 
-    @Test
-    public void testShipment_Sorted_By_Date(){
-        //Implement your solution to get list of shipments
+  /** Sorted shipment by its color in descending order */
+  @Test
+  public void testShipment_Sorted_By_Color_In_Desc() {
+    // Descending order sorting test
+    List<Widget> sortedWidgetsInDescending =
+        shipment.sortWidgetsByColor(
+            new ArrayList<>(widgets), (w1, w2) -> w2.getColor().compareTo(w1.getColor()));
 
+    // test whether the sorted list not holding more than 10 elements.
+    assertTrue(sortedWidgetsInDescending.size() > 0 && sortedWidgetsInDescending.size() <= 10);
 
+    // test the list in descending order by color
+    for (int i = 0; i < sortedWidgetsInDescending.size() - 1; i++) {
+      assertEquals(sortedDescColors.get(i), sortedWidgetsInDescending.get(i).getColor());
+      assertTrue(
+          sortedWidgetsInDescending
+                  .get(i)
+                  .getColor()
+                  .compareTo(sortedWidgetsInDescending.get(i + 1).getColor())
+              >= 0);
     }
+  }
 
+  /** Sorted shipment by its date in ascending order */
+  @Test
+  public void testShipment_Sorted_By_Date() {
+    // Ascending order sorting test
+    List<Widget> sortedWidgetsInAscending =
+        shipment.sortWidgetsByDate(new ArrayList<>(widgets), null);
 
+    // test whether the sorted list not holding more than 10 elements.
+    assertTrue(sortedWidgetsInAscending.size() > 0 && sortedWidgetsInAscending.size() <= 10);
+
+    // test the list in ascending order by date
+    for (int i = 0; i < sortedWidgetsInAscending.size() - 1; i++) {
+      assertEquals(sortedAscDates.get(i), sortedWidgetsInAscending.get(i).getProductionDate());
+
+      assertTrue(
+          sortedWidgetsInAscending
+                  .get(i)
+                  .getProductionDate()
+                  .compareTo(sortedWidgetsInAscending.get(i + 1).getProductionDate())
+              <= 0);
+    }
+  }
+
+  /** Sorted shipment by its date in descending order */
+  @Test
+  public void testShipment_Sorted_By_Date_In_Desc() {
+    // Descending order sorting test
+    List<Widget> sortedWidgetsInDescending =
+        shipment.sortWidgetsByDate(
+            new ArrayList<>(widgets),
+            (w1, w2) -> w2.getProductionDate().compareTo(w1.getProductionDate()));
+
+    // test whether the sorted list not holding more than 10 elements.
+    assertTrue(sortedWidgetsInDescending.size() > 0 && sortedWidgetsInDescending.size() <= 10);
+
+    // test the list in descending order by date
+    for (int i = 0; i < sortedWidgetsInDescending.size() - 1; i++) {
+      assertEquals(sortedDescDates.get(i), sortedWidgetsInDescending.get(i).getProductionDate());
+
+      assertTrue(
+          sortedWidgetsInDescending
+                  .get(i)
+                  .getProductionDate()
+                  .compareTo(sortedWidgetsInDescending.get(i + 1).getProductionDate())
+              >= 0);
+    }
+  }
 }
